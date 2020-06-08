2e70a5b15322b688bb8267ac4638b0e63d941d99
Herman Wu <herman.wu@live.com>
2020-06-08 09:03:48 +0800

    more parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh -l
 
-echo "Hello $1"
+echo "Hello $1 $2 $3 $4"
 time=$(date)
 echo ::set-output name=time::$time
 
+DBURL=$1
+TOKEN=$2
+CLUSTERID=$3
+WORKSPACEPATH=$4
+
+echo "Hello2 $DBURL $TOKEN $CLUSTERID $WORKSPACEPATH"
 
 python3 ${SCRIPTPATH}/executenotebook.py --workspace=${DBURL}\
                         --token=$TOKEN\
