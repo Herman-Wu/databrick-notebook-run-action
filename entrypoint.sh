1bfdf3621f0b52de8ce987a72f6d62aea2a2ef76
Herman Wu <herman.wu@live.com>
2020-06-09 10:33:45 +0800

    fix wrong path

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,6 @@ echo "Hello2 $DBURL $TOKEN $CLUSTERID $NOTEBOOKPATH"
 python3 ${SCRIPTPATH}/executenotebook.py --workspace=${DBURL}\
                         --token=$TOKEN\
                         --clusterid=${CLUSTERID}\
-                        --localpath=/github/workspace/${LOCALPATH}\
+                        --localpath=${LOCALPATH}\
                         --workspacepath=${WORKSPACEPATH}\
                         --outfilepath=${OUTFILEPATH}
