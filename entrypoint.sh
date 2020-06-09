2097cc19a2ebbc77b245b5b04fd70d267fd64936
Herman Wu <herman.wu@live.com>
2020-06-09 10:40:22 +0800

    fix path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-echo "Hello $1 $2 $3 $4 $5"
+echo "Hello $1 $2 $3 $4 $5 $6"
 time=$(date)
 echo ::set-output name=time::$time
 
@@ -9,9 +9,9 @@ TOKEN=$2
 CLUSTERID=$3
 LOCALPATH=$4
 WORKSPACEPATH=$5
+OUTFILEPATH=$6
 
-
-echo "Hello2 $DBURL $TOKEN $CLUSTERID $NOTEBOOKPATH"
+echo "Hello2 $DBURL $TOKEN $CLUSTERID $LOCALPATH $WORKSPACEPATH $OUTFILEPATH"
 
 python3 ${SCRIPTPATH}/executenotebook.py --workspace=${DBURL}\
                         --token=$TOKEN\
